17eca226b86e8b9831ca2db818bff081284f68ba
Deepak Choudhary <gljat999@gmail.com>
2019-10-22 18:54:19 +0530

    IfElse program added

---
+++ IfElse.java
@@ -0,0 +1,18 @@
+public class IfElse{
+    public static void main(String[] args) {
+        
+        int v1 = 10;
+        int v2 = 20;
+
+        if (v1 > v2) {
+            System.out.println("V1 is Bigger");
+        }
+        else if (v2 > v1) {
+            System.out.println("V2 is Bigger");
+        }
+        else 
+            System.out.println("V1 and V2 Both Are equal..."); 
+    
+            
+    }
+}
