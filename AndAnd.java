430fd892ba9d606fb514370a998d0a03a5c23fc6
Deepak Choudhary <gljat999@gmail.com>
2019-10-22 21:55:26 +0530

    AndAnd program added

---
+++ AndAnd.java
@@ -0,0 +1,17 @@
+public class AndAnd
+{
+    public static void main(String[] args) {
+        
+        int student = 120;
+        int rooms = 0;
+
+        if(rooms > 0 && student/rooms > 30)   //if(rooms > 0 & student/rooms > 30)
+        {
+            System.out.println("Crowed...:-(");
+        }
+        else
+            System.out.println("Not Crowed... :-)");
+
+        System.out.println("Program Ended... :-(");
+    }
+}
