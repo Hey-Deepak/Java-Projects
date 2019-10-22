7f632f2359c0809f784110dabfe8825d86b21d98
Deepak Choudhary <gljat999@gmail.com>
2019-10-22 23:04:47 +0530

    for looooop

---
+++ For.java
@@ -0,0 +1,16 @@
+public class For
+{
+    public static void main(String[] args) {
+        
+        
+        for(int val1 = 5; val1 < 100; val1 *= 2 )
+        {
+            System.out.print(val1);
+            System.out.print("* 2 = ");
+            
+            System.out.println(val1);
+        }
+        
+        System.out.println("Happy Ending.. :-)");
+    }
+}
