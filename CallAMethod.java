544e38a052b973d9a4966de35142da9dddc37167
Deepak Choudhary <gljat999@gmail.com>
2019-11-16 17:17:46 +0530

    CallAMethod Added

---
+++ CallAMethod.java
@@ -0,0 +1,8 @@
+public class CallAMethod {
+    static void myMethod(){
+        System.out.println("Hello World, How is the Josh!!!\nIf it is low, its your Bad.\nMake it High");
+    }
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
