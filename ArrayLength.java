57e5e53b7236c56ef617bbd7b4884b455e67516f
Deepak Choudhary <gljat999@gmail.com>
2019-11-16 15:17:37 +0530

    ArrayLength Added

---
+++ ArrayLength.java
@@ -0,0 +1,6 @@
+public class ArrayLength {
+    public static void main(String[] args) {
+        String[] myName = {"Deepak", "Choudhary", "Babu", "Suraj", "Narad"};
+        System.out.println(myName.length);
+    }
+}
