65aa2f75b0bb2760cf025c889e42a343655a1128
Deepak Choudhary <gljat999@gmail.com>
2019-11-16 18:32:37 +0530

    MyClass Uploaded

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    int x = 10;
+    public static void main(String[] args) {
+
+        MyClass myObject = new MyClass();
+        System.out.println(myObject.x);
+    }
+    
+}
