fc8b52eea2268c50e336c29853e2c3c36ee69752
Deepak Choudhary <gljat999@gmail.com>
2019-11-16 15:25:39 +0530

    ForEachLoopWithArray Added

---
+++ ForEachLoopWithArray.java
@@ -0,0 +1,8 @@
+public class ForEachLoopWithArray {
+    public static void main(String[] args) {
+        String[] myName = {"Deepak", "Choudhary", "Babu", "Suraj", "Narad"};
+        for(String i : myName) {
+        System.out.println(i);
+        }
+    }
+}
