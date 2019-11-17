5a96a4b37392294aeaf909ef347cfeefea546c5b
Deepak Choudhary <gljat999@gmail.com>
2019-11-18 02:36:03 +0530

    Scanner added

---
+++ Scannerr.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+ public class Scannerr {
+    public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    String yourName = myObj.nextLine();
+    System.out.println("Enter Name:-");
+    System.out.println("Your Name is " + yourName);
+ }
+}
