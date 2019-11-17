a1e253f8ebc3209452cfce88ac4085d37a4d2cf6
Deepak Choudhary <gljat999@gmail.com>
2019-11-18 01:00:46 +0530

    Encapsulation added

---
+++ MyClass1.java
@@ -0,0 +1,7 @@
+public class MyClass1 {
+    public static void main(String[] args) {
+        Person1 myObj = new Person1();
+        myObj.setName("Deepak");
+        System.out.println(myObj.getName());
+    }
+}
---
+++ Person1.java
@@ -0,0 +1,10 @@
+public class Person1 {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+}
