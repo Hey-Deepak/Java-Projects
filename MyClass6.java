569290c8bb7a0a82ba178af34c55c1a33b948385
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-19 22:39:44 +0530

    Wrapper classes added

---
+++ MyClass6.java
@@ -0,0 +1,7 @@
+public class MyClass6{
+    public static void main(String[] args) {
+        Integer myInt = 1000;
+        String myString = myInt.toString();
+        System.out.println("String Length:- " + myString.length());
+    }
+}
