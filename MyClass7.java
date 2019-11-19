4e2fb8ae7cc3278cc7a1cc2c4ff68861917892a3
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-19 23:28:36 +0530

    Try and Catch added

---
+++ MyClass7.java
@@ -0,0 +1,11 @@
+public class MyClass7{
+    public static void main(String[] args) {
+        try {
+            int[] myNmber = {10, 20, 30, 40};
+            System.out.println(myNmber[10]);
+            
+        } catch (Exception e) {
+            System.out.println("Something Went Wrong...");
+        }
+    }
+}
