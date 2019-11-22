1629edb86e8c532ef80556f203b4f95854a3ec7c
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 08:09:54 +0530

    overwrite a value

--- W3 Practice/DC2.java
+++ W3 Practice/DC2.java
@@ -5,6 +5,7 @@ public class DC2 {
     public static void main(String[] args) {
         DC2 myInfo = new DC2();
         myInfo.myName = "HeyDeepak";
+        myInfo.myAge = 20;
         myInfo.myAge = 21;
         System.out.println("My Name is " + myInfo.myName + " and My Age is " + myInfo.myAge);
     }
