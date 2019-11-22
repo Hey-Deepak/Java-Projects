2aa67781ab01d3a1986f82746e73836c860f2643
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 15:43:47 +0530

    finding string in another string

---
+++ W3 Practice/DC13.java
@@ -0,0 +1,8 @@
+public class DC13 {
+    public static void main(String[] args) {
+        String txt =  "My name is Deepak Choudhary ";
+        System.out.println(txt.indexOf("Deepak"));
+        System.out.println("Length of the String :- " + txt.length());
+
+    }
+}
