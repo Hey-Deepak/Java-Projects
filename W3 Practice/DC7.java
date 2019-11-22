55c27ad58677d95a49cb134f2f3de68d2b83177f
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 15:07:04 +0530

    widening casting added

---
+++ W3 Practice/DC7.java
@@ -0,0 +1,8 @@
+public class DC7 {
+    public static void main(String[] args) {
+        int myInt = 21;
+        double myDouble = myInt;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+}
