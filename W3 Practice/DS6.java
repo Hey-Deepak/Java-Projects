68028a793639de071b267aba9d3858ef447f429f
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 10:03:21 +0530

    Diffrent Data type added

---
+++ W3 Practice/DS6.java
@@ -0,0 +1,14 @@
+public class DS6 {
+    public static void main(String[] args) {
+        int myInt = 999;
+        String myName = "Deepak";
+        boolean myBool = true;
+        long myLong = 315424234;
+        double myDouble = 1212.121212d;
+        System.out.println(myInt);
+        System.out.println(myName);
+        System.out.println(myLong);
+        System.out.println(myBool);
+        System.out.println(myDouble);
+    }
+}
