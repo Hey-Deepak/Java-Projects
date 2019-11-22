728dd61af6f60467a7a7c442bcb4745a0d8c5428
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-23 03:31:42 +0530

    return value returned means added

---
+++ W3 Practice/DC31.java
@@ -0,0 +1,10 @@
+public class DC31 {
+    public int add(int x, int y){
+        int z = x + y;
+        return z;
+    }
+    public static void main(String[] args) {
+        DC31 myObj = new DC31();
+        System.out.println(myObj.add(20, 20));
+    }
+}
