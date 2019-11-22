004f61b38bcdf38159f3fb413022a43f5c2b664b
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 17:53:34 +0530

    Break keyword used

---
+++ W3 Practice/DC22.java
@@ -0,0 +1,11 @@
+public class DC22 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
