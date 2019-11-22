746201c2f4fd07de3a5d400d4af247a7e02badf8
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 15:58:55 +0530

    updated

--- W3 Practice/DC15.java
+++ W3 Practice/DC15.java
@@ -1,8 +1,10 @@
 public class DC15 {
     public static void main(String[] args) {
-        boolean cooding = false;
+        int cooding = 10 ;
+        boolean coooding = false;
         boolean coding = true;
-        System.out.println(cooding);
+        System.out.println(cooding == 10);
         System.out.println(coding);
+        System.out.println(coooding);
     }
 }
