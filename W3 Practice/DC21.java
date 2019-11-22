bf4e388cecfe9e243c9ba7f53d88cc050f725c32
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 17:12:28 +0530

    For Each Added

---
+++ W3 Practice/DC21.java
@@ -0,0 +1,8 @@
+public class DC21 {
+    public static void main(String[] args) {
+        String[] name = {"Deepak", "Choudhary", "JECRC University"};
+        for (String i : name) {
+            System.out.println(i);
+        }
+    }
+}
