dafadec9d0df04fce7c8b5f507519c8694db7bcc
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 16:20:51 +0530

    Switch case added to my WorkSpace

---
+++ W3 Practice/DC17.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class DC17 {
+    public static void main(String[] args) {
+        char myChar = '-';
+        int a = 10;
+        int b = 20;
+    
+        switch (myChar) {
+            case '+': 
+                System.out.println(a + b);
+                break;
+            case '-': 
+                System.out.println(a - b);
+                break;
+            case '/': 
+                System.out.println(a / b);
+                break;    
+            default:
+                System.out.println("Something went Wrong...");
+                
+        }
+    }
+}
