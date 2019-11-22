db9c507663bd2d8bfeb6cd59e32088c5b8acf315
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-23 03:13:26 +0530

    mathod with parameters added

---
+++ W3 Practice/DC29.java
@@ -0,0 +1,10 @@
+public class DC29 {
+    public void name(String x){
+        System.out.println("My name is " + x);
+    }
+    public static void main(String[] args) {
+        DC29 myObj = new DC29();
+        myObj.name("Deepak");
+        myObj.name("DC");
+    }
+}
