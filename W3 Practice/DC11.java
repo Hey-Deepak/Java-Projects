165a2b251a86a8b0228e5d01578f93020e7a1ab0
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 15:35:19 +0530

    Length of String  updated

--- W3 Practice/DC11.java
+++ W3 Practice/DC11.java
@@ -2,5 +2,7 @@ public class DC11 {
     public static void main(String[] args) {
         String myName =  "Deepak";
         System.out.println(myName);
+        System.out.println("Length of the String :- " + myName.length());
+
     }
 }
