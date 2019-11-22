ee77ae9290a98cd7f0dcec925a58f05f32599663
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-23 03:04:03 +0530

    created a method and callled

---
+++ W3 Practice/DC28.java
@@ -0,0 +1,9 @@
+public class DC28 {
+    public void name() {
+        System.out.println("I am Method");
+    };
+    public static void main(String[] args) {
+        DC28 myObj = new DC28();
+        myObj.name();
+    }
+}
