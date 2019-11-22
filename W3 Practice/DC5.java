740cef0535c0adba2809e172d586ea4bf26df066
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 09:45:30 +0530

    adding threw two variable

---
+++ W3 Practice/DC5.java
@@ -0,0 +1,9 @@
+public class DC5 {
+    String myName = "Deepak";
+    Integer myAge = 21;
+    public static void main(String[] args) {
+        DC5 myObj = new DC5();
+      //  DC5 myObj1 = new DC5()
+        System.out.println("Name :- " + myObj.myName + "\nAge :- " + myObj.myAge );
+    }
+}
