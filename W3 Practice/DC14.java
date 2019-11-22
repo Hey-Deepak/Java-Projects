954fea2b178277b5e1d38a771f0a83813d56b8ad
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 15:50:42 +0530

    Random and abs and Sqrt added

--- W3 Practice/DC14.java
+++ W3 Practice/DC14.java
@@ -3,5 +3,9 @@ public class DC14 {
         int myNum1 =  12;
         int myNum2 = 21;
         System.out.println(Math.max(myNum1, myNum2));
+        System.out.println(Math.min(myNum1, myNum2));
+        System.out.println(Math.abs(myNum1));
+        System.out.println(Math.random());
+        System.out.println(Math.sqrt(myNum1));
     }
 }
