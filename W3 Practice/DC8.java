a624fcbed4c6a3624fa5fafb741a60c3f26f8ee0
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 15:18:59 +0530

    Narrowing casting added

---
+++ W3 Practice/DC8.java
@@ -0,0 +1,8 @@
+public class DC8 {
+    public static void main(String[] args) {
+        double myDouble = 12.12d;
+        int myInt = (int)myDouble;
+        System.out.println(myDouble);
+        System.out.println(myInt);    
+    }
+}
