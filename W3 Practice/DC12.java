30722ddb4b3fe54d69909c16d4ee25c584fe53da
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 15:37:20 +0530

    Upper and Lower case added

---
+++ W3 Practice/DC12.java
@@ -0,0 +1,9 @@
+public class DC12 {
+    public static void main(String[] args) {
+        String myName =  "Deepak";
+        System.out.println(myName.toLowerCase());
+        System.out.println(myName.toUpperCase());
+        System.out.println("Length of the String :- " + myName.length());
+
+    }
+}
