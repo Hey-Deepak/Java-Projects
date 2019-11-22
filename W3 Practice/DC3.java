5baf36ae69af0a7d5312b015d8346a88a342b017
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 09:27:40 +0530

    final program added and overide modied

--- W3 Practice/DC3.java
+++ W3 Practice/DC3.java
@@ -1,13 +1,14 @@
 public class DC2 {
     String myName;
     Integer myAge;
+    final int myFinal = 21;
 
     public static void main(String[] args) {
         DC2 myInfo = new DC2();
         myInfo.myName = "HeyDeepak";
         myInfo.myAge = 20;
-        myInfo.myAge = 21;
-        System.out.println("My Name is " + myInfo.myName + " and My Age is " + myInfo.myAge);
+        myInfo.myFinal = myInfo.myAge;
+        System.out.println("My Name is " + myInfo.myName + " and My Age is " + myInfo.myFinal);
     }
 
 }
