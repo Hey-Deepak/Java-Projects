303cfa87f4b6434ee131c78a68e595da5af34caa
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 15:46:57 +0530

    Math.max added

---
+++ W3 Practice/DC14.java
@@ -0,0 +1,7 @@
+public class DC14 {
+    public static void main(String[] args) {
+        int myNum1 =  12;
+        int myNum2 = 21;
+        System.out.println(Math.max(myNum1, myNum2));
+    }
+}
