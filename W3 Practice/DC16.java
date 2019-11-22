2c7ea20e850548e51ca712791a3405cd107b2848
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 16:04:04 +0530

    If and Else program added

---
+++ W3 Practice/DC16.java
@@ -0,0 +1,11 @@
+public class DC16 {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a > 5) {
+            System.out.println("I am in if Condition");
+            a++;
+        } else {
+            System.out.println("I am in Else Condition");
+        }
+    }
+}
