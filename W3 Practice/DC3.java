fd75fd6988c4882ff7c556286ee9d87d4a6548e5
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 14:59:01 +0530

    changes added

--- W3 Practice/DC3.java
+++ W3 Practice/DC3.java
@@ -1,13 +1,13 @@
-public class DC2 {
+public class DC3 {
     String myName;
     Integer myAge;
     final int myFinal = 21;
 
     public static void main(String[] args) {
-        DC2 myInfo = new DC2();
+        DC3 myInfo = new DC3();
         myInfo.myName = "HeyDeepak";
         myInfo.myAge = 20;
-        myInfo.myFinal = myInfo.myAge;
+        myInfo.myFinal = myInfo.myAge; // Cannot assign a value to final variable. 
         System.out.println("My Name is " + myInfo.myName + " and My Age is " + myInfo.myFinal);
     }
 
