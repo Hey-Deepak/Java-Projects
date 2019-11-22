5c37a6aa9746fe00668830d5b3ba1ad631ee1558
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 17:00:18 +0530

    Do While added

---
+++ W3 Practice/DC19.java
@@ -0,0 +1,9 @@
+public class DC19 {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+    }
+}
