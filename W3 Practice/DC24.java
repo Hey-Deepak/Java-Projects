ec16fd6b43ab17f7085a78029eec96de23d2f7d3
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-23 02:17:06 +0530

    array element changed

--- W3 Practice/DC24.java
+++ W3 Practice/DC24.java
@@ -1,6 +1,7 @@
 public class DC24 {
     public static void main(String[] args) {
         String[] myName = {"Deepak", "Babu", "Suraj"};
+        myName[2] = "DC";
         for (String i : myName) {
         System.out.println(i);
         }
