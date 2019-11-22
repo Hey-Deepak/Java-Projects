e819a18696146d60aec2a58b300459c7c73a9135
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 08:01:29 +0530

    Integer variable added

--- W3 Practice/DC1.java
+++ W3 Practice/DC1.java
@@ -1,6 +1,9 @@
 public class DC1 {
     public static void main(String[] args) {
         String myName = "HeyDeepak";
+        Integer age = 21;
         System.out.println(myName);
+        System.out.println(age);
+
     }
 }
