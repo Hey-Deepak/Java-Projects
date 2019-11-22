f2753a686c923757ba73cf54f6c7b910bced45e8
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 09:32:20 +0530

    Combined variable and text together

---
+++ W3 Practice/DC4.java
@@ -0,0 +1,8 @@
+public class DC4 {
+    String myName = "Deepak";
+
+    public static void main(String[] args) {
+        DC4 myObj = new DC4();
+        System.out.println(myObj.myName + " Choudhary");
+    }
+}
