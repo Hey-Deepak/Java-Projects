e4fbb9b150d7d150fa2856b825b8855c088e4901
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 15:27:24 +0530

    Logical Operator added

---
+++ W3 Practice/DC10.java
@@ -0,0 +1,8 @@
+public class DC10 {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+        System.out.println(x > 2 && y > 5);
+        System.out.println(x > 6 || y > 2);
+    }
+}
