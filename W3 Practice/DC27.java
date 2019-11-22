af6189c70f8616a041ea5b33054999432d91bf63
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-23 02:44:13 +0530

    loop through multidimensional array added

---
+++ W3 Practice/DC27.java
@@ -0,0 +1,11 @@
+public class DC27 {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2}, {3, 4}};
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.println(a[i][j]);
+            }
+            
+        }
+     }
+}
