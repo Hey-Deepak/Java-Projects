4bc8051be5efc6191bd987206ec4675d4f50003e
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 16:28:57 +0530

    While loop added

---
+++ W3 Practice/DC18.java
@@ -0,0 +1,9 @@
+public class DC18 {
+    public static void main(String[] args) {
+        int a = 12;
+        while (a > 10) {
+            System.out.println("your are in while loop");
+            a--;
+        }
+    }
+}
