a12e25494821daabcea5c045323204fafbd45887
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-23 02:13:56 +0530

    created and accessed array

---
+++ W3 Practice/DC24.java
@@ -0,0 +1,8 @@
+public class DC24 {
+    public static void main(String[] args) {
+        String[] myName = {"Deepak", "Babu", "Suraj"};
+        for (String i : myName) {
+        System.out.println(i);
+        }
+    }
+}
