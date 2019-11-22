d341d235d4ff247cebbc7fc2ca54c0e15158f191
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-22 08:08:32 +0530

    Created a variable without assigning them

---
+++ W3 Practice/DC2.java
@@ -0,0 +1,12 @@
+public class DC2 {
+    String myName;
+    Integer myAge;
+
+    public static void main(String[] args) {
+        DC2 myInfo = new DC2();
+        myInfo.myName = "HeyDeepak";
+        myInfo.myAge = 21;
+        System.out.println("My Name is " + myInfo.myName + " and My Age is " + myInfo.myAge);
+    }
+
+}
