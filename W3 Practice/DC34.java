d230493c58f4cba6bccf662ad174b2c1dbc1bf6e
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-23 13:54:11 +0530

    created a class and an object of its class.

---
+++ W3 Practice/DC34.java
@@ -0,0 +1,10 @@
+public class DC34 {
+    String name;
+
+    public static void main(String[] args) {
+        DC34 myObj = new DC34();
+        myObj.name = "Deepak";
+        System.out.println(myObj.name);
+
+    }
+}
