c87823bba499eee8955e51e7605174b1df8db7eb
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-23 14:03:50 +0530

    class method created

---
+++ W3 Practice/DC35.java
@@ -0,0 +1,9 @@
+public class DC35 {
+    public void myFunction(){
+        System.out.println("I am Function");
+    }
+    public static void main(String[] args) {
+        DC35 myObj = new DC35();
+        myObj.myFunction();
+    }
+}
