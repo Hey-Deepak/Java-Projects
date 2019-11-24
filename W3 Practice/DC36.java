3b978fd3921fdc3bcd7b0b49800cfe7406785618
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-24 19:25:26 +0530

    access class mathod with an object

---
+++ W3 Practice/DC36.java
@@ -0,0 +1,9 @@
+public class DC36 {
+    public void myFunction(String name){
+        System.out.println("I am " + name);
+    }
+    public static void main(String[] args) {
+        DC36 myObj = new DC36();
+        myObj.myFunction("Deepak");
+    }
+}
