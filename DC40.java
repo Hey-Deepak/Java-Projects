0a71c05b764b7cfb8b42328abd1c5b57b32d295f
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-27 11:46:24 +0530

    Read a file

--- DC40.java
+++ DC40.java
@@ -1,6 +1,6 @@
 import java.io.File;
-import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class DC40 {
     public static void main(String[] args) {
@@ -10,7 +10,7 @@ public class DC40 {
             String data = myReader.nextLine();
             System.out.println(data);
         } catch (FileNotFoundException e) {
-            System.out.println("AN Error Occurred");
+            System.out.println("An error Occurred...");
         }
     }
 }
